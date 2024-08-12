b398c3ba4f91b50547b7af2bd1694ac1f5960521
Mohab <mr.2m0055@gmail.com>
2024-08-12 19:49:11 +0300

    writing the docs, part I

--- objects.py
+++ objects.py
@@ -1,86 +1,168 @@
 class Wallet:
+    """
+    This class is the standard class for any summation between
+    the other classes
+
+    this was not included in the 'Person' class since addition
+    does not need for a 'name' parameter which is a part in
+    'Person' class
+    """
     def __init__(self, pay:int):
+        """
+        initializing the class
+
+        :param 'pay': the amount of paid money that will be summed
+        :param type: int 
+        """
         self.pay = pay
+
 
     @property
     def pay(self):
         return self._pay
-    
+
+
     @pay.setter
     def pay(self, pay:int):
-        if type(pay) != int:
-            raise ValueError("You surly don't pay this.")
-        
-        self._pay = pay
+        if (type(pay) != int) | (pay < 0):
+            print("You surly don't pay this.")
+            self.pay = input("Tell me again how much did he/she paid? ")
+        else:
+            self._pay = pay
+
 
     def __add__(self, other):
-        return Wallet(self.pay + other.pay)
+        try:
+            return Wallet(self.pay + other.pay)
+        except:
+            raise Exception("Code Error")
 
 
 class Person(Wallet):
+    """
+    the main interface set for the to be used classes
+    """
     def __init__(self, name:str, pay:int):
+        """
+        initializing the class
+
+        :param 'name': the name of the payer
+        :param type: str or None
+
+        :param 'pay': the amount of paid money that will be summed
+        :param type: int 
+        """
         super().__init__(pay)
         self.name = name
+
+
+    # the variable to set the unknow "x" name with
+    UNKX = 1
 
     @property
     def name(self):
         return self._name
-    
+
+
     @name.setter
     def name(self, name:str):
-        if not name:
-            global unkx
-            name = f"unknown {unkx}"
-            unkx += 1
+        if (not name) | (type(name) != str):
+            name = f"unknown {Person.UNKX}"
+            Person.UNKX += 1
         
         self._name = name
 
 
 class Farer(Person):
-    def __init__(self, name:str, pay:int, price:int, quantity:int|None):
+    """
+    This class is used for the simple calculations for the fare modes
+    """
+    def __init__(self, name:str, pay:int, price:int, quantity:int):
+        """
+        initializing the class
+
+        :param 'name': the name of the payer
+        :param type: str or None
+
+        :param 'pay': the amount of paid money that will be summed
+        :param type: int
+
+        :param 'price': the price of the unit of thing to pay for
+        :param type: int
+
+        :param 'quantity': the amount that person payed for
+        :param type: int
+        """
         super().__init__(name, pay)
         self.amount = quantity
         self.charge = price
-    
+
+
     @property
     def charge(self):
         return self._charge
-    
+
+
     @charge.setter
     def charge(self, price):
         x = self.pay - self.amount * price
         if x < 0:
-            raise ValueError("You are will be in Debt")
-        self._charge = x
-    
+            print("You are will be in Debt")
+            self.pay = int(input("How much did he pay you told me? "))
+            self.amount = int(input("And you told me how much?"))
+            self.charge = int(input("Finally, what was the price?"))
+        else:
+            self._charge = x
+
+
     @property
     def amount(self):
         return self._amount
-    
+
+
     @amount.setter
-    def quantity(self, quantity):
+    def amount(self, quantity: int):
         if quantity == None:
             quantity = 1
-        self._amount = quantity
+        elif type(quantity) != int:
+            self.amount = int(input("Wait what? How much again?"))
+        else:
+            self._amount = quantity
 
 
 class Menue(Person):
+    """
+    This class is made for the Restaurant mode, it made to work assist with the complexity of
+    ordering for a group
+    """
     def __init__(self, name: str):
+        """
+        initializing the class
+
+        :param 'name': the name of the payer
+        :param type: str or None
+        """
+        # paying is a matter of later time
         super().__init__(name, 0)
         self.order = {}
 
-    menu = {}
+    # the place to store the price of all kinds ordered in the order
+    MENU = {}
 
     @classmethod
-    def add_item(cls, item:str, price:int):
-        cls.menu[item] = price
+    def add_item(cls, item:str):
+        try:
+            price = int(input(f"Price of {item}? "))
+            cls.MENU[item] = price
+        except:
+            print('You guys pay this?\nagain')
+            cls.add_item(item)
 
-    def add_order(self, item, quantity):
-        if item not in Menue.menu:
-            price = int(input(f"price of {item}?"))
-            Menue.add_item(item, price)
+
+    def add_order(self, item:str, quantity:int):
+        if item.capitalize() not in Menue.MENU:
+            Menue.add_item(item)
         if item in self.order:
             self.order[item] += quantity
         else:
             self.order[item] = quantity
-unkx = 1
